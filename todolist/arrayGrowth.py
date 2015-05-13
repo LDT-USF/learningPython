d641c1af9d259bea72f56699b4504bd786a79f75
LDT-USF <cdarnold@usf.edu>
2015-05-13 13:18:25 -0400

    Added two dimensional array growth

--- todolist/arrayGrowth.py
+++ todolist/arrayGrowth.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 
-myArray = []
-myArray.append(42)
+myArray = [[]]
+myArray[0].append(42)
 
-print(myArray[0])
+print(myArray[0][0])
+
+myArray.append([])
+myArray[1].append(1492)
+
+print(myArray[1][0])
