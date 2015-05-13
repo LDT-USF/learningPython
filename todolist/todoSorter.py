a22addd11c0ce4a5df10ba6bc74db705f0ea9365
LDT-USF <cdarnold@usf.edu>
2015-05-13 13:31:49 -0400

    Files read into two dimensional array

--- todolist/todoSorter.py
+++ todolist/todoSorter.py
@@ -2,12 +2,28 @@
 
 import fileinput
 
+todoList = [[]]
+counter = 0
+
 for line in fileinput.input():
      field = line.split(',')
-
-
-     print(field[1])
      date = list(field[1])
-     print(date[2])
+     day = int(date[2]+date[3])
+     month = int(date[0]+date[1])
+     year = int(date[4]+date[5])
+
+     if(counter > 0):
+          todoList.append([])
+
+     todoList[counter].append(int(field[0]))
+     todoList[counter].append(day)
+     todoList[counter].append(month)
+     todoList[counter].append(year)
+     todoList[counter].append(field[2].rstrip('\n'))
+
+     counter += 1
 
 fileinput.close()
+
+for i in range(counter):
+     print(todoList[i])
